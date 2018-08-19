3e12c772abe7dcc02664c91e88d71a2403755383
amastin-microcenter <41840975+amastin-microcenter@users.noreply.github.com>
2018-08-19 09:32:34 -0500

    Update generalSales.js

--- generalSales.js
+++ generalSales.js
@@ -4,29 +4,29 @@ function lineBuilder(rawSales){
     // while (rawSales.length > 0){
         let currentLine = rawSales[0];
 
-        let transactionID = currentLine.slice(0, currentLine.indexOf(""));
+        let transactionID = currentLine.slice(0, currentLine.indexOf(" "));
         console.log(transactionID);
         currentLine = currentLine.replace(transactionID, "");
 
-        let transactionType = currentLine.slice(0, currentLine.indexOf(""));
+        let transactionType = currentLine.slice(0, currentLine.indexOf(" "));
         console.log(transactionType);
         currentLine = currentLine.replace(transactionType, "");
 
-        let lineNumber = currentLine.slice(0, currentLine.indexOf(""));
+        let lineNumber = currentLine.slice(0, currentLine.indexOf(" "));
         console.log(lineNumber);
         currentLine = currentLine.replace(lineNumber, "");
 
-        let SKU = currentLine.slice(0, currentLine.indexOf(""));
+        let SKU = currentLine.slice(0, currentLine.indexOf(" "));
         console.log(SKU);
         currentLine = currentLine.replace(SKU, "");
 
         let description = currentLine.slice(0, currentLine.indexOf("$"));
         console.log(description);
-        
-        
-        
-        
-        
+
+
+
+
+
     // }
 }
 
