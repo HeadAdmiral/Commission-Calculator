a17922e12839b2e681bc7692c805a063006b12bc
amastin-microcenter <41840975+amastin-microcenter@users.noreply.github.com>
2018-08-19 10:09:26 -0500

    Update generalSales.js

--- generalSales.js
+++ generalSales.js
@@ -1,37 +1,35 @@
 function lineBuilder(rawSales){
     rawSales = rawSales.split(/\r?\n/);
-
-    // while (rawSales.length > 0){
-        let currentLine = rawSales[0];
-        console.log(currentLine);
+    let currentLine = rawSales[0];
+     while (currentLine.length > 0){
+        //console.log(currentLine);
         let transactionID = currentLine.slice(0, currentLine.indexOf(" "));
         console.log(transactionID);
         currentLine = currentLine.replace(transactionID + " ", "");
-        console.log(currentLine);
+        //console.log(currentLine);
 
         let transactionType = currentLine.slice(0, currentLine.indexOf(" "));
         console.log(transactionType);
         currentLine = currentLine.replace(transactionType + " ", "");
-        console.log(currentLine);
+        //console.log(currentLine);
 
         let lineNumber = currentLine.slice(0, currentLine.indexOf(" "));
         console.log(lineNumber);
         currentLine = currentLine.replace(lineNumber + " ", "");
-        console.log(currentLine);
+        //console.log(currentLine);
 
         let SKU = currentLine.slice(0, currentLine.indexOf(" "));
         console.log(SKU);
         currentLine = currentLine.replace(SKU + " ", "");
-        console.log(currentLine);
+        //console.log(currentLine);
 
         let description = currentLine.slice(0, currentLine.indexOf("$"));
-        console.log(description);
-        console.log(currentLine);
+        //console.log(currentLine);
         description = description.slice(0, description.lastIndexOf(" "));
         description = description.slice(0, description.lastIndexOf(" "));
         console.log(description);
         currentLine = currentLine.replace(description + " ", "");
-        console.log(currentLine);
+        //console.log(currentLine);
         let quantity = currentLine.slice(0, currentLine.indexOf(" "));
         console.log(quantity);
         currentLine = currentLine.replace(quantity + " ", "");
@@ -39,7 +37,7 @@ function lineBuilder(rawSales){
         price = currentLine.slice(0, currentLine.indexOf(" "));
         currentLine = currentLine.replace(price + " ", "");
         currentLine = currentLine.replace(price + " ", "");
-        console.log(currentLine);
+        //console.log(currentLine);
         console.log(price);
 
 
