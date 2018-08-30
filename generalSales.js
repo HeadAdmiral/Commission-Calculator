240b1084ad5140023924bf53718c611d44086d12
amastin-microcenter <41840975+amastin-microcenter@users.noreply.github.com>
2018-08-29 21:54:28 -0500

    Update generalSales.js

--- generalSales.js
+++ generalSales.js
@@ -5,56 +5,75 @@ function isLastElement(parentElement){
 function lineBuilder(rawSales){
     rawSales = rawSales.split(/\r?\n/);
     let currentLine = rawSales[0];
-    let counter = 0;
     while (!(isLastElement(currentLine))){
-        //console.log(currentLine);
-        let transactionID = currentLine.slice(0, currentLine.indexOf(" "));
-        console.log(transactionID);
-        currentLine = currentLine.replace(transactionID + " ", "");
-        //console.log(currentLine);
-
-        let transactionType = currentLine.slice(0, currentLine.indexOf(" "));
-        console.log(transactionType);
-        currentLine = currentLine.replace(transactionType + " ", "");
-        //console.log(currentLine);
-
-        let lineNumber = currentLine.slice(0, currentLine.indexOf(" "));
-        console.log(lineNumber);
-        currentLine = currentLine.replace(lineNumber + " ", "");
-        //console.log(currentLine);
-
-        let SKU = currentLine.slice(0, currentLine.indexOf(" "));
-        console.log(SKU);
-        currentLine = currentLine.replace(SKU + " ", "");
-        //console.log(currentLine);
-
-        let description = currentLine.slice(0, currentLine.indexOf("$"));
-        //console.log(currentLine);
-        description = description.slice(0, description.lastIndexOf(" "));
-        description = description.slice(0, description.lastIndexOf(" "));
-        console.log(description);
-        currentLine = currentLine.replace(description + " ", "");
-        //console.log(currentLine);
-        let quantity = currentLine.slice(0, currentLine.indexOf(" "));
-        console.log(quantity);
-        currentLine = currentLine.replace(quantity + " ", "");
-        let price = currentLine.slice(0, currentLine.indexOf(" "));
-        price = currentLine.slice(0, currentLine.indexOf(" "));
-        currentLine = currentLine.replace(price + " ", "");
-        currentLine = currentLine.replace(price + " ", "");
-        //console.log(currentLine);
-        console.log(price);
-        console.log("==========");
-        console.log(currentLine);
-        console.log("==========");
-        counter++;
         
-        let line = {transactionID: transactionID};
+        // Where the information for the current line is:
+        // 45-PO-6934344 Sale 5 480772 CAT 5e Snagless Network Cable 10 ft. - Green 1 $9.99 $9.99
+        
+        // ==================
+        //   TRANSACTION ID
+        // ==================       
+        let transactionID = currentLine.slice(0, currentLine.indexOf(" ")); // 45-PO-6934344
+        currentLine = currentLine.replace(transactionID + " ", ""); // Sale 5 480772 CAT 5e Snagless Network Cable 10 ft. - Green 1 $9.99 $9.99
+
+        // ==================
+        //  TRANSACTION TYPE
+        // ==================    
+        let transactionType = currentLine.slice(0, currentLine.indexOf(" ")); // Sale
+        currentLine = currentLine.replace(transactionType + " ", ""); // 5 480772 CAT 5e Snagless Network Cable 10 ft. - Green 1 $9.99 $9.99
+
+        // ==================
+        //     LINE NUMBER
+        // ==================    
+        let lineNumber = currentLine.slice(0, currentLine.indexOf(" ")); // 5
+        currentLine = currentLine.replace(lineNumber + " ", ""); // 480772 CAT 5e Snagless Network Cable 10 ft. - Green 1 $9.99 $9.99
+        
+        // ==================
+        //        SKU
+        // ==================      
+        let SKU = currentLine.slice(0, currentLine.indexOf(" ")); // 480772
+        currentLine = currentLine.replace(SKU + " ", ""); // CAT 5e Snagless Network Cable 10 ft. - Green 1 $9.99 $9.99
+
+        // ==================
+        //    DESCRIPTION
+        // ==================    
+        let description = currentLine.slice(0, currentLine.indexOf("$")); // CAT 5e Snagless Network Cable 10 ft. - Green 1 $9.99
+        description = description.slice(0, description.lastIndexOf(" ")); // CAT 5e Snagless Network Cable 10 ft. - Green 1
+        description = description.slice(0, description.lastIndexOf(" ")); // CAT 5e Snagless Network Cable 10 ft. - Green
+        currentLine = currentLine.replace(description + " ", ""); // 1 $9.99 $9.99
+
+        // ==================
+        //      QUANTITY
+        // ==================    
+        let quantity = currentLine.slice(0, currentLine.indexOf(" ")); // 1
+        currentLine = currentLine.replace(quantity + " ", ""); // $9.99 $9.99
+        
+        // ==================
+        //        PRICE
+        // ==================    
+        let price = currentLine.slice(0, currentLine.indexOf(" ")); // $9.99
+        price = currentLine.slice(0, currentLine.indexOf(" ")); //
+        currentLine = currentLine.replace(price + " ", "");
+        currentLine = currentLine.replace(price + " ", "");
+        
+        let line = {
+            transactionID: transactionID,
+            transactionType: transactionType,
+            lineNumber: lineNumber,
+            sku: SKU,
+            desc: description,
+            quantity: quantity,
+            price: price
+        };
         console.log(line.transactionID);
+        console.log(line.transactionType);
+        console.log(line.lineNumber);
+        console.log(line.sku);
+        console.log(line.desc);
+        console.log(line.quantity);
+        console.log(line.price);
         console.log("==========");
      }
-
-        
 }
 
 let salesInfo;
