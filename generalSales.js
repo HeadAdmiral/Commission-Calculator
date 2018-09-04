f4b88666e854794f004f54fd45142838be61a06c
cd3770rn <cd3770rn@go.minneapolis.edu>
2018-09-04 17:37:56 -0500

    Updated generalSales.js

--- generalSales.js
+++ generalSales.js
@@ -1,28 +1,22 @@
-let salesData;
-
 function formatPrice(price) {
     // If price is negative, change from ($29.99) to -29.99
-    if (price.indexOf("(") != -1){
+    if (price.indexOf("(") !== -1){
         price = price.replace("(", "");
         price = price.replace("$", "-");
         price = price.replace(")", "");
-        price = Number(price)
-        price = price.toFixed(2);
     }
 
     // If price is positive, change from $29.99 to 29.99
     else{
         price = price.replace("$", "");
-        price = Number(price)
-        price = price.toFixed(2);
     }
-    
+
     return price;
 }
 
 function isLastElement(parentElement){
     // Returns true if parentElement does not contain spaces (ie. there are no elements after the current one)
-    return (!(parentElement.indexOf(" ") != -1))
+    return (!(parentElement.indexOf(" ") !== -1))
 }
 
 function lineBuilder(rawSales){
@@ -31,7 +25,7 @@ function lineBuilder(rawSales){
     let lineItems = [];
     while (!(isLastElement(currentLine))){
         
-        // Where the information for the current line is:
+        // ex. Where the information for the current line is:
         // 45-PO-6934344 Sale 5 480772 CAT 5e Snagless Network Cable 10 ft. - Green 1 $9.99 $9.99
         
         // ==================
@@ -100,37 +94,357 @@ function calculateCommission(lineItem){
     const COMMISSION_UNDER_TEN = 0.06; // 6%
     const COMMISSION_UNDER_HUNDRED = 0.03; // 3%
     const COMMISSION_OVER_HUNDRED = 0.015; // 1.5%
-    let commission;
-    
-    // If not a service plan
-    // If not a TV
-    
-    // Maybe make a method that determines what kind of item the lineItem is, and then go from there in this method.
-    if (lineItem.price > 0 && lineItem.price <= 9.99) {
-        commission = lineItem.price * COMMISSION_UNDER_TEN;
-    }
-    else if (lineItem.price > 9.99 && lineItem.price <= 99.99) {
-        commission = lineItem.price * COMMISSION_UNDER_HUNDRED;
-    }
-    else if (lineItem.price > 99.99) {
-        commission = lineItem.price * COMMISSION_OVER_HUNDRED;
-    }
+    const COMMISSION_SERVICE_PLAN = 0.1; // 10%
+    let commission = 0;
 
-} 
+    if (isNotServicePlan(lineItem) && isNotTV(lineItem)) {
 
-function getItemsUnderTen(lineItems) {
-    for (let i = 0; i < lineItems.length; i++){
-        let currentItem = lineItems[i].price
-        if (currentItem > 0 && currentItem <= 9.99) {
-            console.log(lineItems[i].price);
+        if (lineItem.price < 0){
+            if (lineItem.price < 0 && lineItem.price >= -9.99) {
+                commission = lineItem.price * COMMISSION_UNDER_TEN;
+            }
+            else if (lineItem.price < -9.99 && lineItem.price > -99.99) {
+                commission = lineItem.price * COMMISSION_UNDER_HUNDRED;
+            }
+            else if (lineItem.price > 99.99) {
+                commission = lineItem.price * COMMISSION_OVER_HUNDRED;
+            }
+        }
+
+        else {
+            // Maybe make a method that determines what kind of item the lineItem is, and then go from there in this method.
+            if (lineItem.price > 0 && lineItem.price <= 9.99) {
+                commission = lineItem.price * COMMISSION_UNDER_TEN;
+            }
+            else if (lineItem.price > 9.99 && lineItem.price <= 99.99) {
+                commission = lineItem.price * COMMISSION_UNDER_HUNDRED;
+            }
+            else if (lineItem.price > 99.99) {
+                commission = lineItem.price * COMMISSION_OVER_HUNDRED;
+            }
         }
     }
+
+    else {
+        if (isServicePlan(lineItem)) {
+            commission = lineItem.price * COMMISSION_SERVICE_PLAN;
+        }
+
+        else if (isTV(lineItem)) {
+            commission = lineItem.price * COMMISSION_OUT_OF_DEPARTMENT;
+        }
+
+    }
+
+    return commission;
 }
 
+function getCommission(lineItems){
+    commission = 0;
+    for (let i = 0; i < lineItems.length; i++){
+        commission += calculateCommission(lineItems[i]);
+    }
+    return commission;
+}
+
+function getItemsUnderTen(lineItems) {
+    let itemsUnderTen = [];
+
+    for (let i = 0; i < lineItems.length; i++){
+        if (isUnderTen(lineItems[i])) {
+            itemsUnderTen.push(lineItems[i]);
+        }
+    }
+
+    return itemsUnderTen;
+}
+
+function getItemsUnderHundred(lineItems){
+    let itemsUnderHundred = [];
+
+    for (let i = 0; i < lineItems.length; i++){
+        if (isUnderHundred(lineItems[i])) {
+            itemsUnderHundred.push(lineItems[i]);
+        }
+    }
+
+    return itemsUnderHundred;
+
+}
+
+function getItemsOverHundred(lineItems){
+    let itemsOverHundred = [];
+
+    for (let i = 0; i < lineItems.length; i++){
+        if (isOverHundred(lineItems[i])) {
+            itemsOverHundred.push(lineItems[i]);
+        }
+    }
+
+    return itemsOverHundred;
+
+}
+
+function getService(lineItems){
+    let service = [];
+
+    for (let i = 0; i < lineItems.length; i++){
+        if (isServicePlan(lineItems[i])) {
+            service.push(lineItems[i]);
+        }
+    }
+    return service;
+}
+
+function getReturns(lineItems){
+    let returns = [];
+
+    for (let i = 0; i < lineItems.length; i++){
+        if (isReturn(lineItems[i])) {
+            returns.push(lineItems[i]);
+        }
+    }
+
+    return returns;
+
+}
+
+function getExchanges(lineItems){
+    let exchanges = [];
+
+    for (let i = 0; i < lineItems.length; i++){
+        if (isExchange(lineItems[i])) {
+            exchanges.push(lineItems[i]);
+        }
+    }
+
+    return exchanges;
+
+}
+
+function isUnderTen(lineItem){
+    return lineItem.price > 0 && lineItem.price <= 9.99 && isNotServicePlan(lineItem) && (!isExchange(lineItem)) && (!isReturn(lineItem));
+}
+
+function isUnderHundred(lineItem){
+    return lineItem.price > 9.99 && lineItem.price <= 99.99 && isNotServicePlan(lineItem) && (!isExchange(lineItem)) && (!isReturn(lineItem));
+}
+
+function isOverHundred(lineItem){
+    return lineItem.price >= 100.00 && isNotServicePlan(lineItem) && (!isExchange(lineItem)) && (!isReturn(lineItem));
+}
+
+function isNotServicePlan(lineItem){
+    // Returns true if item description does not contain the following words:
+    return lineItem.desc.indexOf("Replacement Plan") === -1 &&
+           lineItem.desc.indexOf("replacement plan") === -1;
+}
+
+function isServicePlan(lineItem){
+    return (!isNotServicePlan(lineItem));
+}
+
+function isTV(lineItem){
+    return (!isNotTV(lineItem));
+}
+
+function isNotTV(lineItem){
+    // If item description contains TV...
+    if (lineItem.desc.indexOf("TV" !== -1)) {
+        // Returns false if item is a TV, and not a TV accessory.
+        return isTVAccessory(lineItem);
+    }
+    return true;
+}
+
+function isTVAccessory(lineItem){
+    // Returns true if the item description contains any of the following words:
+    return lineItem.desc.indexOf("Mount") === -1 ||
+           lineItem.desc.indexOf("mount") === -1 ||
+           lineItem.desc.indexOf("Antenna") === -1 ||
+           lineItem.desc.indexOf("antenna") === -1 ||
+           lineItem.desc.indexOf("Antennae") === -1 ||
+           lineItem.desc.indexOf("antennae") === -1 ||
+           lineItem.desc.indexOf("Box") === -1 ||
+           lineItem.desc.indexOf("box") === -1 ||
+           lineItem.desc.indexOf("Cable") === -1 ||
+           lineItem.desc.indexOf("cable") === -1 ||
+           lineItem.desc.indexOf("Tuner") === -1 ||
+           lineItem.desc.indexOf("tuner") === -1
+}
+
+function isReturn(lineItem){
+    return lineItem.transactionType === "Return" && isNotServicePlan(lineItem);
+}
+
+function isExchange(lineItem){
+    return lineItem.transactionType === "Exchange" && isNotServicePlan(lineItem);
+}
+
+function toPercent(a, b){
+    return ((a / b) * 100).toFixed(2) + '%';
+}
+
+function toDollars(a){
+    return a < 0 ? "-$" + (a * -1).toFixed(2) : '$' + a.toFixed(2);
+}
+
+function toggleTextBox(){
+    let textbox = document.getElementById("lunchlength");
+    textbox.disabled = !textbox.disabled;
+}
+
+function getPool(hoursWorked){
+    return hoursWorked * 2;
+}
+
+function getHourly(hoursWorked){
+    return hoursWorked * 4;
+}
+
+function adjustHoursWorked(hoursWorked, lunchLength){
+    hoursWorked -= (lunchLength / 60);
+    return hoursWorked;
+}
 
 function printme(){
-    salesData = document.getElementsByName("sales")[0].value;
+    let salesData = document.getElementsByName("sales")[0].value;
+    let hoursWorked = document.getElementsByName("hoursworked")[0].value;
+    let lunchLength = document.getElementsByName("lunchlength")[0].value;
+    let lunchTaken = document.getElementsByName("lunched")[0].checked;
+
+    let debug = true;
+
+    // If user is scheduled for 8 hours and takes an hour-long lunch, they have worked 7 hours.
+    if (lunchTaken){
+        hoursWorked = adjustHoursWorked(hoursWorked, lunchLength);
+    }
+
+    // ===============
+    // ==== SALES ====
+    // ===============
     let lineItems = lineBuilder(salesData);
-    console.log(lineItems);
-    let itemsUnderTen = getItemsUnderTen(lineItems)
+    let itemsUnderTen = getItemsUnderTen(lineItems);
+    let itemsUnderHundred = getItemsUnderHundred(lineItems);
+    let itemsOverHundred = getItemsOverHundred(lineItems);
+
+    // ===============
+    // === RETURNS ===
+    // ===============
+    let returns = getReturns(lineItems);
+
+    // ===============
+    // == EXCHANGES ==
+    // ===============
+    let exchanges = getExchanges(lineItems);
+
+    // ===============
+    // === SERVICE ===
+    // ===============
+    let service = getService(lineItems);
+
+    // ===============
+    // ==== $$$$$ ====
+    // ===============
+    let commissionUnderTen = getCommission(itemsUnderTen);
+    let commissionUnderHundred = getCommission(itemsUnderHundred);
+    let commissionOverHundred = getCommission(itemsOverHundred);
+    let commissionService = getCommission(service);
+    let commissionReturns = getCommission(returns);
+    let commissionExchanges = getCommission(exchanges);
+    let commissionPool = getPool(hoursWorked);
+    let earnedHourly = getHourly(hoursWorked);
+    let totalEarnedWages = commissionUnderTen +
+                           commissionUnderHundred +
+                           commissionOverHundred +
+                           commissionService +
+                           commissionReturns +
+                           commissionExchanges +
+                           commissionPool +
+                           earnedHourly;
+    let hourlyWage = toDollars(totalEarnedWages / hoursWorked);
+
+    // ===============
+    // = SALES STATS =
+    // ===============
+    let totalTransactions = lineItems.length;
+    let totalSalesUnderTen = itemsUnderTen.length;
+    let totalSalesUnderHundred = itemsUnderHundred.length;
+    let totalSalesOverHundred = itemsOverHundred.length;
+    let totalReturns = returns.length;
+    let totalExchanges = exchanges.length;
+    let totalService = service.length;
+    let percentSalesUnderTen = toPercent(totalSalesUnderTen, totalTransactions);
+    let percentSalesUnderHundred = toPercent(totalSalesUnderHundred, totalTransactions);
+    let percentSalesOverHundred = toPercent(totalSalesOverHundred, totalTransactions);
+    let percentReturns = toPercent(totalReturns, totalTransactions);
+    let percentExchanges = toPercent(totalExchanges, totalTransactions);
+    let percentService = toPercent(totalService, totalTransactions);
+
+    // ===============
+    // ==== DEBUG ====
+    // ===============
+    if (debug) {
+        console.log("===== COMMISSION CALCULATOR =====");
+        console.log("Hours worked: " + hoursWorked);
+        console.log("Lunch taken: " + lunchTaken);
+        if (lunchTaken){
+            console.log("Lunch length: " + lunchLength + " minutes");
+        }
+        console.log("Sales Information:");
+        console.log(lineItems);
+        console.log("============= SALES =============");
+        console.log("Under $10:");
+        console.log(itemsUnderTen);
+        console.log("Under $100:");
+        console.log(itemsUnderHundred);
+        console.log("Over $100:");
+        console.log(itemsOverHundred);
+        console.log("============ RETURNS ============");
+        console.log("Returns:");
+        console.log(returns);
+        console.log("=========== EXCHANGES ===========");
+        console.log("Exchanges:");
+        console.log(exchanges);
+        console.log("============ SERVICE ============");
+        console.log("Service:");
+        console.log(service);
+        console.log("============= STATS =============");
+        console.log("Total transactions: " + totalTransactions);
+        console.log("Total sales under $10: " + totalSalesUnderTen);
+        console.log("Total sales under $100: " + totalSalesUnderHundred);
+        console.log("Total sales over $100: " + totalSalesOverHundred);
+        console.log("");
+        console.log("Percentage of sales under $10: " + percentSalesUnderTen);
+        console.log("Percentage of sales under $100: " + percentSalesUnderHundred);
+        console.log("Percentage of sales over $100: " + percentSalesOverHundred);
+        console.log("");
+        console.log("Total service plans: " + totalService);
+        console.log("Total returns: " + totalReturns);
+        console.log("Total exchanges: " + totalExchanges);
+        console.log("");
+        console.log("Percentage of service plans to transactions: " + percentService);
+        console.log("Percentage of returns to transactions: " + percentReturns);
+        console.log("Percentage of exchanges to transactions: " + percentExchanges);
+        console.log("======= EARNED COMMISSION =======");
+        console.log("Under $10: " + toDollars(commissionUnderTen));
+        console.log("Under $100: " + toDollars(commissionUnderHundred));
+        console.log("Over $100: " + toDollars(commissionOverHundred));
+        console.log("Service: " + toDollars(commissionService));
+        console.log("Returns: " + toDollars(commissionReturns));
+        console.log("Exchanges: " + toDollars(commissionExchanges));
+        console.log("");
+        console.log("Earned hourly: " + toDollars(earnedHourly) + "(" + hoursWorked + "hr(s) worked at $4/hr)");
+        console.log("Estimated pool commission: " + toDollars(commissionPool) + "(" + hoursWorked + "hr(s) worked at ~$2/hr");
+        console.log("Total earned wages: " + toDollars(totalEarnedWages));
+        console.log("============= TOTALS ============");
+        console.log(toDollars(commissionUnderTen + commissionUnderHundred + commissionOverHundred) + "(Sales) + " +
+                    toDollars(commissionService) + "(Service Plans) + " +
+                    toDollars(commissionPool) + "(Estimated Pool) + " +
+                    toDollars(earnedHourly) + "(Earned Hourly) + " +
+                    toDollars(commissionReturns) + "(Returns) + " +
+                    toDollars(commissionExchanges) + "(Exchanges) + " +
+                    " = " + toDollars(totalEarnedWages));
+        console.log(toDollars(totalEarnedWages) + " / " + hoursWorked + "hr(s) worked = " + hourlyWage + "/hr");
+    }
 }
