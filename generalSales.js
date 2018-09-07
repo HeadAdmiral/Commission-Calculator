3c22424a5d622c2407461a07856de2c3a444f42c
amastin-microcenter <41840975+amastin-microcenter@users.noreply.github.com>
2018-09-07 14:33:45 -0500

    Update generalSales.js

--- generalSales.js
+++ generalSales.js
@@ -464,7 +464,7 @@ function toggleOutput(hourlyWage) {
     let input = document.getElementById("form-content");
     let output = document.getElementById("output");
     let commission = document.getElementById("commission");
-    
+     
     input.style.display = "none";
     commission.innerText = hourlyWage;
     output.style.display = "block";
