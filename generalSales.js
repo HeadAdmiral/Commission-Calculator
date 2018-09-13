817f03b49dab13c6acae43ccf7a5b4cf0a70bc30
amastin-microcenter <41840975+amastin-microcenter@users.noreply.github.com>
2018-09-13 15:11:14 -0500

    Update generalSales.js

--- generalSales.js
+++ generalSales.js
@@ -450,7 +450,7 @@ function processSales(){
 	salesStatsCommissionExchanges.innerText = toDollars(commissionExchanges);
 	salesStatsCommissionService.innerText = toDollars(commissionService);
 	salesStatsHourlyRate.innerText = " (" + hoursWorked + "hr(s) at $4/hr)";
-	//salesStatsBaseHourly.innerHTML = toDollars(earnedHourly);
+	salesStatsBaseHourly.innerHTML = toDollars(earnedHourly) + salesStatsHourlyRate.innerText;
 	salesStatsPoolRate.innerText = " (" + hoursWorked + "hr(s) at ~$2/hr)";
 	//salesStatsCommissionPool.innerText = toDollars(commissionPool);
 // 	salesStatsTotalEarnings.innerText = toDollars(totalEarnedWages);
